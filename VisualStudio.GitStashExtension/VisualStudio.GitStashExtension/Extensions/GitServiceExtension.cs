c733d2c5c5d9cd8fc182c1aa3f7b30cd4a695e2b
Victor Voshchinskiy <voshchinskiyvitya@ukr.net>
2018-03-02 15:05:10 +0200

    Issue #4: 'Stashes' button appears on non-Git repo

---
+++ VisualStudio.GitStashExtension/VisualStudio.GitStashExtension/Extensions/GitServiceExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
+
+namespace VisualStudio.GitStashExtension.Extensions
+{
+    /// <summary>
+    /// Extension class for IGitExt.
+    /// </summary>
+    public static class GitServiceExtension
+    {
+        /// <summary>
+        /// Checks existence of any active git repository.
+        /// </summary>
+        public static bool AnyActiveRepository(this IGitExt service)
+        {
+            var activeRepository = service.ActiveRepositories.FirstOrDefault();
+
+            return activeRepository != null;
+        }
+    }
+}
